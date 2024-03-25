0489538c28ea4c13fa8634228845c1328f3aa43b
Mgo Yeghiaian <mgoyeghiaian@gmail.com>
2024-03-26 00:55:02 +0200

    Added Sentry

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
 import Model from "./components/Model";
+import * as Sentry from '@sentry/react';
 const App = () => {
 
   return (
@@ -15,4 +16,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7d7c0afb4c1fcfadfc381d7dc4fdd09c@o4506974000775168.ingest.us.sentry.io/4506974006411264",
+  integrations: [
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
