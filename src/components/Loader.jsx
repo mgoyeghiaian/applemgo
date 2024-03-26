6cab03bc45ed1250a4b1a102e3339d7a7db564ea
Mgo Yeghiaian <mgoyeghiaian@gmail.com>
2024-03-27 00:29:46 +0200

    Added Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,17 +1,19 @@
-import { Html } from '@react-three/drei'
+import { Html } from '@react-three/drei';
 
 const Loader = () => {
+  // Assuming '/public/assets/images/apple.svg' is accessible via a public URL path
+  const logoUrl = '/public/assets/images/apple.svg'; // Adjust the path as needed based on how your app serves static files
+
   return (
     <Html>
-      <div className=' absolute top-0 left-0 w-full h-full flex justify-center items-center'>
-        <div className='w-[10vw] h-[10vw]
-         rounded-full
-        '>
-          Loading...
+      <div className="flex-col gap-4 w-full flex items-center justify-center">
+        <div className="w-28 h-28 border-8 text-blue-400 text-4xl animate-spin border-zinc flex items-center justify-center border-t-gray-300 rounded-full">
+          {/* Use img tag to load the SVG */}
+          <img src={logoUrl} alt="Loading..." className="animate-ping" style={{ fill: 'currentColor', height: '1em', width: '1em' }} />
         </div>
       </div>
     </Html>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
